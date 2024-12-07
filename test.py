15c2b54dd3f280b867eecaac9b262e8d562d7132
MinChanJu <mcj00220077@gmail.com>
2024-12-08 04:29:13 +0900

    vd

--- test.py
+++ test.py
@@ -1,26 +1,8 @@
 import sys
-input = sys.stdin.read
+input = sys.stdin.readline
 
-def solve():
-    n = int(input().strip())
-    
-    for i in range(1, n):
-        print(f"? {i}")
-        sys.stdout.flush()
-        response1 = int(input().strip())
-        
-        print(f"? {i+1}")
-        sys.stdout.flush()
-        response2 = int(input().strip())
-        
-        if response1 == 0 and response2 == 1:
-            print(f"! {i}")
-            sys.stdout.flush()
-            return
-    
-    # 조건을 만족하는 (i, i+1) 쌍이 없는 경우
-    print("! -1")
-    sys.stdout.flush()
+answer = []
 
-if __name__ == "__main__":
-    solve()
+
+
+print(*answer,sep="\n")
